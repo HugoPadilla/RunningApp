f7cb6e1a376e0c5b8787bf446629806bf2aa94e6
Hugo Padilla <hugojavier19890@gmail.com>
2022-05-31 11:47:45 -0500

    Add: Architectural Skeleton (MVVM)

---
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/StatisticsFragment.kt
@@ -0,0 +1,15 @@
+package com.androiddevs.runningappyt.ui.fragments
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.androiddevs.runningappyt.R
+import com.androiddevs.runningappyt.ui.viewModels.MainViewModel
+import com.androiddevs.runningappyt.ui.viewModels.StatisticsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
+
+    private val viewModel: StatisticsViewModel by viewModels()
+
+}
---
+++ app/src/main/java/com/androiddevs/runningappyt/ui/viewModels/StatisticsViewModel.kt
@@ -0,0 +1,13 @@
+package com.androiddevs.runningappyt.ui.viewModels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.androiddevs.runningappyt.repositories.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsViewModel @Inject constructor(
+    val mainRepository: MainRepository
+) : ViewModel() {
+}
