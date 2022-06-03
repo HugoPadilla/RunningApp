cffd5379ca1b6d191d831f303895f81f0f4b4230
Hugo Padilla <hugojavier19890@gmail.com>
2022-06-03 01:26:05 -0500

    Add: Handling the Map's lifecycle

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,74 @@
 package com.androiddevs.runningappyt.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.ui.viewModels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_tracking.*
+
 
 @AndroidEntryPoint
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+class TrackingFragment : Fragment(R.layout.fragment_tracking), OnMapReadyCallback {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private lateinit var map: GoogleMap
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        map = p0
+
+        // Add a marker in Sydney and move the camera test
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
