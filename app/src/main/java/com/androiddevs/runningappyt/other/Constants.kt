9f49e64ea39b5e3bc6d04110e67dac381174b2fd
Hugo Padilla <hugojavier19890@gmail.com>
2022-06-12 13:25:24 -0500

    Tracking User Location in the Background

--- app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
@@ -10,8 +10,11 @@ object Constants {
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
     const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
 
-    val NOTIFICATION_CHANNEL_ID = "tracking_channel"
-    val NOTIFICATION_CHANNEL_NAME = "Tracking"
-    val NOTIFICATION_ID = 1
+    const val LOCATION_UPDATE_INTERVAL = 5000L
+    const val FASTEST_LOCATION_INTERVAL = 2000L
+
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
 
 }
